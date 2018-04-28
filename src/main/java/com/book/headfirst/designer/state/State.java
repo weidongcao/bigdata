396b29e3d26f2593e79c467a0a7b85534a3a3da2
I75930 <caowd1990@gmail.com>
2018-04-28 22:56:59 +0800

    《Head First 设计模式》第10章状态模式

---
+++ src/main/java/com/book/headfirst/designer/state/State.java
@@ -0,0 +1,11 @@
+package com.book.headfirst.designer.state;
+
+import java.io.Serializable;
+
+public interface State extends Serializable{
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+}
